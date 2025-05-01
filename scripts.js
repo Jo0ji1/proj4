b648447bce51a1dd98a81542d0657bba5469c2b8
George <antwil2050@gmail.com>
2025-04-30 23:54:45 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('cadastro').addEventListener('submit', function(event){
-    event.preventDefault();
-    let cpf = document.getElementById('cpf').value;
-    if(cpf.length !== 11 || isNaN(cpf)){
-        alert('CPF inválido.');
-    }else{
-        alert('Formulário enviado!');
-    }
-});
